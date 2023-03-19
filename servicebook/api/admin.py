b0ea75a8002f63427c8ac020b5bbb7c2c3c3a67e
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2023-03-19 13:41:42 +0500

    Добавил модели в админку

--- servicebook/api/admin.py
+++ servicebook/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Handbook, Vehicle, Maintenance, Reclamation
+
+admin.site.register([Handbook, Vehicle, Maintenance, Reclamation])
