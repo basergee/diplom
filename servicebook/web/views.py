d7b37eb9bfaa6a1e418103aba5e64121b2ff030f
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2023-03-22 23:17:42 +0500

    Ограничил удаление объектов ТО

--- servicebook/web/views.py
+++ servicebook/web/views.py
@@ -271,11 +271,36 @@ class MaintenanceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView)
         return False
 
 
-class MaintenanceDeleteView(LoginRequiredMixin, DeleteView):
+class MaintenanceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = 'delete_object_form.html'
     model = Maintenance
     login_url = reverse_lazy('login')
     success_url = reverse_lazy('maintenance')
+
+    def test_func(self):
+        user = self.request.user
+
+        if user.groups.filter(name='Manager').exists():
+            # Менеджер может удалить любой объект
+            return True
+
+        # Узнаем ключ объекта, который пытаемся удалить
+        maintenance_id = int(self.request.path.split('/')[-2])
+
+        # Чтобы иметь доступ, пользователь должен быть клиентом, владеющим
+        # машиной, или сервисной компанией, обслуживающей машину
+        if user.groups.filter(name='Client').exists():
+            # Пользователь -- клиент. Проверим, что машина, запись о ТО которой
+            # хочет получить пользователь, принадлежит ему
+            return Maintenance.objects.get(pk=maintenance_id).vehicle.client == user
+
+        if user.groups.filter(name='ServiceCompany').exists():
+            # Пользователь -- сервисная компания. Проверим, что машина, запись
+            # о ТО которой хочет получить пользователь, обслуживается
+            # этой сервисной компанией
+            return Maintenance.objects.get(pk=maintenance_id).service_company == user
+
+        return False
 
 
 class ReclamationView(LoginRequiredMixin, ListView):
